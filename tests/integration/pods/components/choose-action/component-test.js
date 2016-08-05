a969bf92f4eb24f4b806fabab5e6e992c1f588c4
Adriaan <harianus@users.noreply.github.com>
2016-08-05 16:23:58 +0100

    Give the choose-action component test a better name

--- tests/integration/pods/components/choose-action/component-test.js
+++ tests/integration/pods/components/choose-action/component-test.js
@@ -6,7 +6,7 @@ moduleForComponent('choose-action', 'Integration | Component | choose action', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders with and without card', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
