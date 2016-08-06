e9a78df56199ac6467683d45384d14f04b198445
Adriaan <harianus@users.noreply.github.com>
2016-08-06 17:18:00 +0100

    Add translations

---
+++ app/locales/nl/translations.js
@@ -0,0 +1,22 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  card: {
+    got: 'Ik een kaartje <strong>gekregen</strong>',
+    gave: 'Ik een kaartje <strong>gegeven</strong>',
+  },
+  code: {
+    change: 'Wijzig code',
+    name: 'Code'
+  },
+  messages: {
+    title: 'Berichten'
+  }
+};
