ca4efb1cf34d165de664fe2c2c9417faa8c2b2d9
Adriaan <harianus@users.noreply.github.com>
2016-08-06 17:18:37 +0100

    Remove examples from the translations

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -1,13 +1,4 @@
 export default {
-  // "some.translation.key": "Text for some.translation.key",
-  //
-  // "a": {
-  //   "nested": {
-  //     "key": "Text for a.nested.key"
-  //   }
-  // },
-  //
-  // "key.with.interpolation": "Text with {{anInterpolation}}"
   card: {
     got: 'I <strong>got</strong> a card',
     gave: 'I <strong>gave</strong> a card'
