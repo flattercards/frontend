d43241d4d28cab40188c5cd698489dca20aaf489
Adriaan van Rossum <git@adriaan.io>
2016-08-12 23:45:18 +0100

    Add message to change to English

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -6,15 +6,35 @@ const {
 } = Ember;
 
 export default Controller.extend({
+  i18n: Ember.inject.service(),
+  alert: null,
+  from: null,
+
   fetchCountry: on('init', function() {
+    if (this.get('i18n.locale') === 'en') return;
     fetch('http://ipinfo.io', {
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
       }
     }).then(response => {
-      const res = JSON.parse(response._bodyText);
-      console.log(res);
+      const user = JSON.parse(response._bodyText);
+      if (user.country !== 'NL') {
+        this.showChangeToEnglish(user);
+      }
     });
-  })
+  }),
+
+  showChangeToEnglish(user) {
+    const from = user.region || 'outside The Netherlands'
+    this.set('alert', true);
+    this.set('from', from);
+  },
+
+  actions: {
+    changeLang(language) {
+      this.set('alert', false);
+      this.set('i18n.locale', language);
+    }
+  }
 });
