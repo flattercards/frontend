b8040374e6f935dc65ad363c5a9d86e96a7e5d54
Adriaan van Rossum <git@adriaan.io>
2016-08-13 23:59:55 +0100

    Add locale based on user location

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -8,11 +8,29 @@ export default Controller.extend({
   i18n: Ember.inject.service(),
   alert: false,
 
+  saveLocale(locale) {
+    const language = locale.toLowerCase();
+    this.set('i18n.locale', language);
+    if (!window.localStorage || !window.localStorage.setItem) return;
+    window.localStorage.setItem('language', language);
+  },
+
+  getUserData() {
+    return fetch('https://freegeoip.net/json/').then(response => {
+      return response.json();
+    });
+  },
+
   checkLocale: on('init', function() {
     if (!window.localStorage || !window.localStorage.setItem) return;
     const language = window.localStorage.getItem('language');
     if (language) return this.set('i18n.locale', language);
-    this.set('alert', true);
+
+    this.getUserData().then(user => {
+      if (user.country_code === 'NL') return this.saveLocale('nl');
+      if (user.country_code === 'BE') return this.saveLocale('nl');
+      return this.saveLocale('en');
+    });
   }),
 
   actions: {
--- mirage/config.js
+++ mirage/config.js
@@ -31,4 +31,5 @@ export default function() {
 
   this.get('messages/:id');
   this.post('messages');
+  this.passthrough('https://freegeoip.net/json/');
 }
